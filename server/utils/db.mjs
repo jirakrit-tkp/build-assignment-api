0613be5718e7803b286de57d4a0e785e580602dd
Jirakrit T. <jirakrit.tkp@gmail.com>
2025-09-01 13:24:54 +0700

    feat(api): handle assignment creation

--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -4,7 +4,7 @@ const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:20594535Tantann!@localhost:5432/assignments_db",
 });
 
 export default connectionPool;
