18e1e4de75842ee544475eaf62fea7d46f014244
Jirakrit T. <jirakrit.tkp@gmail.com>
2025-09-02 14:58:04 +0700

    feat(api): add CRUD operations for assignments

--- server/app.mjs
+++ server/app.mjs
@@ -10,6 +10,96 @@ app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
 
+app.get("/assignments", async (req,res) => {
+  let result
+  try {
+  result = await connectionPool.query(`select * from assignments`)
+  } catch(error) {
+    return res.status(500).json({
+      message : "Server could not read assignment because database connection"
+    })
+  }
+  return res.status(200).json({
+    data : result.rows
+  })
+});
+
+app.get("/assignments/:assignmentId", async (req,res) => {
+  const assignmentIdFromClient = req.params.assignmentId;
+  let result;
+  try {
+    result = await connectionPool.query(`select * from assignments where assignment_id = $1`,[assignmentIdFromClient]);
+  } catch(error) {
+    return res.status(500).json({
+      message : "Server could not read assignment because database connection"
+    })
+  }
+  if (!result.rows[0]) {
+    return res.status(404).json({
+      message : "Server could not find a requested assignment"
+    })
+  }
+  return res.status(200).json({
+    data : result.rows[0]
+  })
+});
+
+app.put("/assignments/:assignmentId", async (req,res) => {
+  const assignmentIdFromClient = req.params.assignmentId;
+  const newAssignment = {...req.body, updated_at: new Date()}
+  let result;
+  try {
+    result = await connectionPool.query(`select * from assignments where assignment_id = $1`,[assignmentIdFromClient]);
+    await connectionPool.query(`
+      update assignments
+      set title = $1,
+          content = $2,
+          category = $3,
+          updated_at = $4
+      where assignment_id = $5
+      `,[
+        newAssignment.title,
+        newAssignment.content,
+        newAssignment.category,
+        newAssignment.updated_at,
+        assignmentIdFromClient
+      ]);
+  } catch(error) {
+    return res.status(500).json({
+      message : "Server could not read assignment because database connection"
+    })
+  }
+  if (!result.rows[0]) {
+    return res.status(404).json({
+      message : "Server could not find a requested assignment"
+    })
+  }
+  return res.status(200).json({
+    message: "Updated post successfully"
+  })
+});
+
+app.delete("/assignments/:assignmentId", async (req,res) => {
+  const assignmentIdFromClient = req.params.assignmentId;
+  let result;
+  try {
+    result = await connectionPool.query(`select * from assignments where assignment_id = $1`,[assignmentIdFromClient]);
+    await connectionPool.query(`delete from assignments where assignment_id = $1`,[assignmentIdFromClient]);
+  } catch(error) {
+    return res.status(500).json({
+      message : "Server could not read assignment because database connection"
+    })
+  }
+  if (!result.rows[0]) {
+    return res.status(404).json({
+      message : "Server could not find a requested assignment"
+    })
+  }
+  return res.status(200).json({
+    message: "Deleted post successfully"
+  })
+});
+
 app.post("/assignments", async (req,res) => {
   const newAssignment = {
     ...req.body,
